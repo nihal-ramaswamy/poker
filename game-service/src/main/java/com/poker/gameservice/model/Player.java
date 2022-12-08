bd15d4f43359ac3b4f4ac9dbb9e0214654af6772
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2022-12-08 22:00:52 +0530

    Add connection to postgres

---
+++ game-service/src/main/java/com/poker/gameservice/model/Player.java
@@ -0,0 +1,20 @@
+package com.poker.gameservice.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "player")
+public class Player {
+    @Id
+    @SequenceGenerator(name = "users_sequence", sequenceName = "users_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_sequence")
+    private Long playerID;
+
+    @Column(nullable = false, unique = true)
+    private Long currentGameID;
+
+    @Column(nullable = false)
+    private Long currentMoney;
+
+    @Column(nullable = false)
+    private Long moneyInPot;
+}
---
+++ game-service/src/main/java/com/poker/gameservice/types/Card.java
@@ -0,0 +1,8 @@
+package com.poker.gameservice.types;
+
+import lombok.Data;
+@Data
+public class Card {
+    private String suite;
+    private String rank;
+}
