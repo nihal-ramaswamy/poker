1b77f256b81c5e6304b50869a19ac1a3e3ba0db8
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2022-12-08 20:26:21 +0530

    Add RandomStringGenerator

---
+++ game-service/src/main/java/com/poker/gameservice/config/Constants.java
@@ -0,0 +1,5 @@
+package com.poker.gameservice.config;
+
+public class Constants {
+    public static int CODE_LENGTH = 6;
+}
---
+++ game-service/src/main/java/com/poker/gameservice/util/RandomStringGenerator.java
@@ -0,0 +1,22 @@
+package com.poker.gameservice.util;
+
+import com.poker.gameservice.config.Constants;
+import java.util.Random;
+
+public class RandomStringGenerator {
+    public String generate() {
+
+        int codeLen = Constants.CODE_LENGTH;
+
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+
+        while (salt.length() < codeLen) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+
+        return salt.toString();
+    }
+}
