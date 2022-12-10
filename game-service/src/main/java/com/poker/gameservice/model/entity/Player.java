2e5640d99ba429221a10a255b404787ba5746e7a
prithvianilk <thepirateking3d2y@gmail.com>
2022-12-11 01:01:57 +0530

    Add player repository

--- game-service/src/main/java/com/poker/gameservice/model/entity/Player.java
+++ game-service/src/main/java/com/poker/gameservice/model/entity/Player.java
@@ -8,13 +8,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "player")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Player {
     @Id
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
     private String currentGameID;
 
     @Column(nullable = false)
