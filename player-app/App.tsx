11920be1243a22471f97512f32431d42a775f739
prithvianilk <thepirateking3d2y@gmail.com>
2022-12-11 22:11:58 +0530

    Add simple game joining form in player app

--- player-app/App.tsx
+++ player-app/App.tsx
@@ -1,9 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import axios from "./config/axios";
 
 export default function App() {
+  const [gameID, setGameID] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+
+  const joinGame = async () => {
+    const {
+      data: { playerID },
+    } = await axios.post("/game/join", {
+      gameID,
+      playerUsername: username,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Poker App</Text>
+      <View style={styles.labelContainer}>
+        <Text style={styles.label}>Username</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+      />
+      <View style={styles.labelContainer}>
+        <Text style={styles.label}>Game ID</Text>
+      </View>
+      <TextInput style={styles.input} onChangeText={setGameID} value={gameID} />
+      <Button onPress={joinGame} title="Join Game" />
     </View>
   );
 }
@@ -14,5 +40,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  labelContainer: {
+    width: "75%",
+  },
+  label: {
+    fontSize: 18,
+  },
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
